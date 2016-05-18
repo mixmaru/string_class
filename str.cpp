9d61ccbb575529d31bbcdd34cea16795d2b7fc85
mix <mix@mairyouta-no-MacBook-Pro.local>
2016-05-18 22:49:58 +0900

    add実行できた。テストも完了

--- str.cpp
+++ str.cpp
@@ -10,12 +10,13 @@ string::string(){
 
 void string::set(const char *arg_str){
 	//必要ならメモリ領域を追加
-	int need_count = _length(arg_str) + str_count;
+	int add_count = _length(arg_str);
+	int need_count = add_count + str_count;
 
 	if(need_count > max_char_num){
 		//必要な文字数の2倍のメモリ領域を確保(+1はhull文字分)
-		max_char_num = (need_count * 2) + 1;
-		str = new char[max_char_num];
+		max_char_num = (need_count * 2);
+		str = new char[max_char_num + 1];
 		*str = 0;
 	}
 
@@ -28,10 +29,44 @@ void string::set(const char *arg_str){
 		work_arg_str++;
 	}
 	*work_str = '\0';
+
+	//文字カウント更新
+	str_count = str_count + need_count;
 }
 
-void string::add(const char *str){
+void string::add(const char *arg_str){
+	int add_count = _length(arg_str);
+	int need_count = add_count + str_count;
+	if(need_count > max_char_num){
+		//tmp_strに必要量の倍のメモリを確保する
+		max_char_num = need_count * 2;
+		char *tmp_str = new char[max_char_num + 1];
 
+		//strの文字をtmp_strに移す
+		char *work_str = str;
+		char *work_tmp_str = tmp_str;
+		while(*work_str){
+			*work_tmp_str = *work_str;
+			work_tmp_str++;
+			work_str++;
+		}
+		*work_tmp_str = '\0';
+		
+		str = tmp_str;
+	}
+
+	//strの末へ追加していく
+	char *work_str = str + str_count;
+	const char *work_arg_str = arg_str;
+	while(*work_arg_str != '\0'){
+		*work_str = *work_arg_str;
+		work_str++;
+		work_arg_str++;
+	}
+	*work_str = '\0';
+	
+	//文字数カウント更新
+	str_count = str_count + add_count;
 }
 
 string string::extract(int start, int length){
