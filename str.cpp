1b153364a5cd830e301d4fe21ac45fc3302c4786
mix <mix@mairyouta-no-MacBook-Pro.local>
2016-05-26 23:05:56 +0900

    _extract関数を作成した。コメントを配置した

--- str.cpp
+++ str.cpp
@@ -13,6 +13,7 @@ string::~string(){
 	//delete[] m_str;
 }
 
+//文字列をセットする
 void string::set(const char *arg_str){
 	//必要ならメモリ領域を追加
 	int add_count = _length(arg_str);
@@ -29,6 +30,7 @@ void string::set(const char *arg_str){
 	m_count = m_count + need_count;
 }
 
+//文字列を追加する
 void string::add(const char *arg_str){
 	int add_count = _length(arg_str);
 	int need_count = add_count + m_count;
@@ -44,16 +46,11 @@ void string::add(const char *arg_str){
 	m_count = m_count + add_count;
 }
 
+//m_strからstart+1文字目からlimit文字を取り出した新しいstringインスタンスを返す
 string string::extract(int start, int length){
 	//m_strから文字を切り出す
 	char *new_str = new char[length + 1];
-	char *work_str = m_str + start;
-	char *work_new_str = new_str;
-	int i;
-	for(i=0; i<length; i++){
-		*(work_new_str+i) = *(work_str+i);
-	}
-	*(work_new_str+i) = '\0';
+	_extract(m_str, new_str, start, length);
 
 	//stringインスタンスとして作って返す
 	string ret_string;
@@ -62,23 +59,17 @@ string string::extract(int start, int length){
 	return ret_string;
 }
 
+//m_str文字列のポインタを返す
 char *string::value(){
 	return m_str;
 }
 
+//m_countを返す
 int string::length(){
 	return m_count;
 }
 
-int string::_length(const char *arg_str){
-	int count = 0;
-	while(*arg_str){
-		arg_str++;
-		count++;
-	}
-	return count;
-}
-
+//m_strのメモリサイズをwant_limit文字入るように拡張する
 void string::_expandLimit(int want_limit){
 	//tmp_strに必要量の倍のメモリを確保する
 	m_limit = want_limit * 2;
@@ -91,6 +82,17 @@ void string::_expandLimit(int want_limit){
 	m_str = tmp_str;
 }
 
+//文字列定数の文字数を返す
+int string::_length(const char *arg_str){
+	int count = 0;
+	while(*arg_str){
+		arg_str++;
+		count++;
+	}
+	return count;
+}
+
+//string1の文字列の末尾からstring2の文字列を追加する
 void string::_add(char *string1, const char *string2){
 	//string1のポインタを、\0の位置まですすめる
 	while(*string1){
@@ -103,4 +105,16 @@ void string::_add(char *string1, const char *string2){
 		string2++;
 	}
 	*string1 = '\0';
+}
+
+//from_strのstart文字目からlength文字をto_strへコピーする
+void string::_extract(const char *from_str, char *to_str, int start, int length){
+	//start位置までポインタを移動させる
+	from_str = from_str + start;
+	//指定位置の文字を切り出してコピーする
+	int i;
+	for(i=0; i<length; i++){
+		*(to_str + i) = *(from_str + i);
+	}
+	*(to_str + i) = '\0';
 }
--- str.hpp
+++ str.hpp
@@ -6,18 +6,37 @@ private:
 	char *m_str;//文字列を格納する
 	int m_count;//格納された文字数を格納する
 	int m_limit;//格納できる最大文字数を入れる。（確保メモリサイズ-1）
-	void _expandLimit(int want_limit);//m_strのメモリサイズをwant_limit文字入るように拡張する
-	int _length(const char *string);//文字列定数の文字数を返す
-	void _add(char *string1, const char *string2);//string1の文字列の末尾からstring2の文字列を追加する
+
+	//m_strのメモリサイズをwant_limit文字入るように拡張する
+	void _expandLimit(int want_limit);
+
+	//文字列定数の文字数を返す
+	int _length(const char *string);
+
+	//string1の文字列の末尾からstring2の文字列を追加する
+	void _add(char *string1, const char *string2);
+
+	//from_strのstart文字目からlength文字をto_strへコピーする
+	void _extract(const char *from_str, char *to_str, int start, int length);
 	
 public:
 	string();//コンストラクタ
 	~string();//デストラクタ
-	void set(const char *string);//文字列をセットする
-	void add(const char *string);//文字列を追加する
-	string extract(int start, int limit);//m_strからstart+1文字目からlimit文字を取り出した新しいstringインスタンスを返す
-	char *value();//m_str文字列のポインタを返す
-	int length();//m_countを返す
+
+	//文字列をセットする
+	void set(const char *string);
+
+	//文字列を追加する
+	void add(const char *string);
+
+	//m_strからstart+1文字目からlimit文字を取り出した新しいstringインスタンスを返す
+	string extract(int start, int limit);
+
+	//m_str文字列のポインタを返す
+	char *value();
+
+	//m_countを返す
+	int length();
 };
 
 #endif
